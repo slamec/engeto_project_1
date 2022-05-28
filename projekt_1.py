27e6c4a85af7cc7e6ef958358d4680d6c273d66b
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-05-28 21:55:29 +0200

    variables created

--- projekt_1.py
+++ projekt_1.py
@@ -6,3 +6,16 @@ discord: Miro#8969
 
 """
 
+import task_template as texts 
+
+
+user = ['bob', 'mike', 'ann', 'liz']
+password = ['123', 'pass123', 'password123', 'pass123']
+text = texts.TEXTS
+
+user_input = input(str('Hello, please insert your user name and password: '))
+user_input_list = user_input.split(' ')
+
+user_name = user_input_list[0]
+user_pass = user_input_list[1]
+
