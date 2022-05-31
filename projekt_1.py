16417e809cc0983759610d0bbd4747fa08fc207f
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-05-31 18:25:52 +0200

    if statement added

--- projekt_1.py
+++ projekt_1.py
@@ -13,9 +13,11 @@ user = ['bob', 'mike', 'ann', 'liz']
 password = ['123', 'pass123', 'password123', 'pass123']
 text = texts.TEXTS
 
-user_input = input(str('Hello, please insert your user name and password: '))
+user_input = input(str('Hello, please insert your user name and password (separated by whitespace): '))
 user_input_list = user_input.split(' ')
 
 user_name = user_input_list[0]
 user_pass = user_input_list[1]
 
+if user_name in user and user_pass in password:
+    pass 
